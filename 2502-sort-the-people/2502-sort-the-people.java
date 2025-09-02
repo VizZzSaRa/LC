7cbb581143d1925816f990dadfc95ef910ecaea4
Vishnusarathi V <vishnusarathi2410@gmail.com>
2025-09-02 10:44:40 +0530

    Time: 9 ms (46.24%), Space: 45.5 MB (42.67%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        Map<Integer,String> m = new TreeMap<>();
+        for(int i=0;i<names.length;i++){
+            m.put(heights[i],names[i]);
+        }
+        int j=names.length-1;
+       for(int i:m.keySet()){
+        names[j--]=m.get(i);
+       }        
+       return names;
+    }
+}
