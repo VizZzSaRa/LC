e65dd545cce388cf9b55da2b05d97a3d719c9001
Vishnusarathi V <vishnusarathi2410@gmail.com>
2025-09-02 10:47:46 +0530

    Time: 11 ms (36.39%), Space: 45.6 MB (42.67%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -4,10 +4,13 @@ class Solution {
         for(int i=0;i<names.length;i++){
             m.put(heights[i],names[i]);
         }
-        int j=names.length-1;
-       for(int i:m.keySet()){
-        names[j--]=m.get(i);
-       }        
+    //     int j=names.length-1;
+    //    for(int i:m.keySet()){
+    //     names[j--]=m.get(i);
+    //    }    
+    Arrays.sort(heights);
+    int j=names.length-1;
+    for(int i:heights) names[j--]=m.get(i);
        return names;
     }
 }
