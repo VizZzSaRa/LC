cf20d85a34875c488a0ea5ac277d10a784d26b47
Vishnusarathi V <vishnusarathi2410@gmail.com>
2025-10-05 23:52:30 +0530

    Time: 7 ms (78.04%), Space: 45.4 MB (50.8%) - LeetHub

--- 2825-minimize-string-length/2825-minimize-string-length.java
+++ 2825-minimize-string-length/2825-minimize-string-length.java
@@ -1,12 +1,8 @@
 class Solution {
-    public int minimizedStringLength(String s) {
-        int arr[] = new int[26];
-        for(char i:s.toCharArray())
-        arr[i-'a']++;
-        int c=0;
-        for(int i:arr){
-            if(i!=0)c++;
-        }
-        return c;
+    public int minimizedStringLength(String str) {
+        
+        Set<Character> s = new HashSet<>();
+        for(char i:str.toCharArray()) s.add(i);
+        return s.size();
     }
 }
