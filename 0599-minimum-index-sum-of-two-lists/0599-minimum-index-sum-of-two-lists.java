5db7dc54333f4807b3b649a81eecfadb8a342caf
Vishnusarathi V <vishnusarathi2410@gmail.com>
2025-11-11 12:10:31 +0530

    Time: 116 ms (6.17%), Space: 47.6 MB (10.77%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String[] findRestaurant(String[] s1, String[] s2) {
+        Map<String,Integer> m = new LinkedHashMap<>();
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<s1.length;i++){
+            for(int j=0;j<s2.length;j++){
+                if(s1[i].equals(s2[j])){
+                    m.put(s1[i],(i+j));
+                    if(min>(i+j)) min=i+j;
+                }
+            }
+        }
+        List<String> l = new ArrayList<>();
+        for(String i:m.keySet()){
+            if(m.get(i)==min) l.add(i);
+        }
+        String [] arr= new String[l.size()];
+        int k=0;
+        for(String i:l) arr[k++] =i;
+        return arr;
+        
+    }
+}
