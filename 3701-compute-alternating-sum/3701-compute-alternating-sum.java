4955f2391114f16dde3cf52d61ab95f137e2539d
Vishnusarathi V <vishnusarathi2410@gmail.com>
2025-11-22 13:59:16 +0530

    Time: 1 ms (94.51%), Space: 46.4 MB (5.25%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int s=0;
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0) s+=nums[i];
+            else s-=nums[i];
+        }
+        return s;
+    }
+}
